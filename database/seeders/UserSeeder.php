b305d9210755fb5b11b106fd8d71fb3d1d352fe6
alamkajoba <alamkajoba007@gmail.com>
2025-12-23 13:55:55 +0200

    k

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,13 +17,13 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         $user = User::create([
-            'name' => 'Alam kajoba',
-            'identifiant' => 'KAJOBA007',
+            'name' => 'Ibamba GEORGES',
+            'identifiant' => 'identifiant',
             'password' => Hash::make('password')
         ]);
 
 
-        $adminRole = Role::firstOrCreate(['name' => 'Top Admin']);
+        $adminRole = Role::firstOrCreate(['name' => 'Manager']);
         $adminRole->syncPermissions(Permission::all());
         $user->assignRole($adminRole);
     }
